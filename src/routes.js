c003e786faf71b470ce325b7f36d3d2bbd813207
Roberto <roberto_junior_sousa@hotmail.com>
2020-06-04 00:10:52 -0300

    blocks brute force attacks

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,8 @@
 import { Router } from 'express';
 
+import Brute from 'express-brute';
+import BruteKnex from 'brute-knex';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
@@ -7,7 +10,18 @@ import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.post('/sessions', SessionController.store);
+const bruteStore = new BruteKnex({
+  createTable: true,
+});
+
+const bruteForce = new Brute(bruteStore, {
+  freeRetries: 5,
+  minWait: 5 * 60 * 1000, // 5 minutes
+  maxWait: 60 * 60 * 1000, // 1 hour,
+  lifetime: 24 * 60 * 60, // 1 day (seconds not milliseconds)
+});
+
+routes.post('/sessions', bruteForce.prevent, SessionController.store);
 
 routes.use(authMiddleware);
 
